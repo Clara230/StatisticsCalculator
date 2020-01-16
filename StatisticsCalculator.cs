1f66800bae8376a957db8e7adf1b735531ce1f5a
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-16 12:24:03 +0200

    Add project files.

---
+++ StatisticsCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BasicCalculator;
+
+namespace StatisticsCalculator
+{
+    public class StatisticsCalculator  : Calculator
+    {
+        public dynamic Mean(dynamic [] values)
+        {
+            var sum = Add(values);
+            var valueCount = values.Length;
+            result = Divide(sum, valueCount);
+            return result;
+        }
+    }
+}
